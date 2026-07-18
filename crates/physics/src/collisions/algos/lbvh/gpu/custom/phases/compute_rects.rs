1a4c03c1f046e99be3202a3c7dc24ec28815bccd
whyyoudodisjoel <medicalajoel@gmail.com>
2026-07-18 09:49:09 +0530

    fmt

--- crates/physics/src/collisions/algos/lbvh/gpu/custom/phases/compute_rects.rs
+++ crates/physics/src/collisions/algos/lbvh/gpu/custom/phases/compute_rects.rs
@@ -1,9 +1,9 @@
 use codegen::asts::lowered::BindedBuffer;
 use codegen::asts::lowered::Scope;
 use codegen::asts::lowered::scope::*;
+use codegen::{call, group};
 use codegen_macros::{IntoWgslStruct, shader};
 use tinyge_graphics::shaders::ComputeShader;
-use codegen::{call, group};
 
 #[derive(IntoWgslStruct)]
 pub struct Vertex {
@@ -37,51 +37,89 @@ fn compute_rects(
     let info = scope.var("info", global(1).i(local(model_idx).load()).load());
     let model_offset = scope.var("model_offset", local(info).f("offset").load());
     let model_vertex_count = scope.var("model_vertex_count", local(info).f("stride").load());
-    let local_min = scope.mut_("local_min", vec3(f32::INFINITY, f32::INFINITY, f32::INFINITY));
-    let local_max = scope.mut_("local_max", vec3(-f32::INFINITY, -f32::INFINITY, -f32::INFINITY));
+    let local_min = scope.mut_(
+        "local_min",
+        vec3(f32::INFINITY, f32::INFINITY, f32::INFINITY),
+    );
+    let local_max = scope.mut_(
+        "local_max",
+        vec3(-f32::INFINITY, -f32::INFINITY, -f32::INFINITY),
+    );
     let i = scope.mut_("i", local(lid).load());
     let offset = scope.mut_("offset", u32(128));
 
     let body = group!(
-        scope.while_loop(
-        local(model_vertex_count).load().gt(local(i).load()),
-        |b| {
-            let v = b.var("v", model_verts.var_ref().i(local(model_offset).load() + local(i).load()).f("pos").load());
+        scope.while_loop(local(model_vertex_count).load().gt(local(i).load()), |b| {
+            let v = b.var(
+                "v",
+                model_verts
+                    .var_ref()
+                    .i(local(model_offset).load() + local(i).load())
+                    .f("pos")
+                    .load(),
+            );
             b.ast = Some(group!(
-                store(local(local_min), call!("min", local(local_min).load(), local(v).load())),
-                store(local(local_max), call!("max", local(local_max).load(), local(v).load())),
+                store(
+                    local(local_min),
+                    call!("min", local(local_min).load(), local(v).load())
+                ),
+                store(
+                    local(local_max),
+                    call!("max", local(local_max).load(), local(v).load())
+                ),
                 store(local(i), local(i).load() + u32(256)),
             ));
-        },
-    ),
-    store(shared(0).i(local(lid).load()), local(local_min).load()),
-    store(shared(1).i(local(lid).load()), local(local_max).load()),
-    call!("workgroupBarrier"),
-    scope.while_loop(local(offset).load().gt(u32(0)), |b| {
-        let if_ast = b.cond(local(offset).load().gt(local(lid).load()), |ib| {
-            ib.ast = Some(group!(
-                store(shared(0).i(local(lid).load()), call!("min",
-                    shared(0).i(local(lid).load()).load(),
-                    shared(0).i(local(lid).load() + local(offset).load()).load(),
-                )),
-                store(shared(1).i(local(lid).load()), call!("max",
-                    shared(1).i(local(lid).load()).load(),
-                    shared(1).i(local(lid).load() + local(offset).load()).load(),
-                )),
+        },),
+        store(shared(0).i(local(lid).load()), local(local_min).load()),
+        store(shared(1).i(local(lid).load()), local(local_max).load()),
+        call!("workgroupBarrier"),
+        scope.while_loop(local(offset).load().gt(u32(0)), |b| {
+            let if_ast = b.cond(
+                local(offset).load().gt(local(lid).load()),
+                |ib| {
+                    ib.ast = Some(group!(
+                        store(
+                            shared(0).i(local(lid).load()),
+                            call!(
+                                "min",
+                                shared(0).i(local(lid).load()).load(),
+                                shared(0).i(local(lid).load() + local(offset).load()).load(),
+                            )
+                        ),
+                        store(
+                            shared(1).i(local(lid).load()),
+                            call!(
+                                "max",
+                                shared(1).i(local(lid).load()).load(),
+                                shared(1).i(local(lid).load() + local(offset).load()).load(),
+                            )
+                        ),
+                    ));
+                },
+                None::<fn(&mut Scope)>,
+            );
+            b.ast = Some(group!(
+                if_ast,
+                call!("workgroupBarrier"),
+                store(local(offset), local(offset).load() >> u32(1)),
             ));
-        }, None::<fn(&mut Scope)>);
-        b.ast = Some(group!(
-            if_ast,
-            call!("workgroupBarrier"),
-            store(local(offset), local(offset).load() >> u32(1)),
-        ));
-    }),
-    scope.cond(local(lid).load().eq(u32(0)), |b| {
-        b.ast = Some(group!(
-            store(output_rect.var_ref().i(local(model_idx).load()).f("min"), shared(0).i(u32(0)).load()),
-            store(output_rect.var_ref().i(local(model_idx).load()).f("max"), shared(1).i(u32(0)).load()),
-        ));
-    }, None::<fn(&mut Scope)>),
+        }),
+        scope.cond(
+            local(lid).load().eq(u32(0)),
+            |b| {
+                b.ast = Some(group!(
+                    store(
+                        output_rect.var_ref().i(local(model_idx).load()).f("min"),
+                        shared(0).i(u32(0)).load()
+                    ),
+                    store(
+                        output_rect.var_ref().i(local(model_idx).load()).f("max"),
+                        shared(1).i(u32(0)).load()
+                    ),
+                ));
+            },
+            None::<fn(&mut Scope)>
+        ),
     );
     scope.ast = Some(body);
 
